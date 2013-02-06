a089898855cc317163b42d482dd98204d3a190fb
Scott Robinson <scott@quadhome.com>
2013-02-06 14:28:22 +0000

    Run the stage1 babushka inside the bootstrapped environment.

--- karen.rb
+++ karen.rb
@@ -1,24 +1,46 @@
 dep 'bootstrap', :disk do
   disk.default! '/dev/sda'
 
-  requires 'chroot'.with(disk)
+  requires 'stage1.bootstrap'.with(disk)
+
+  met? {
+    log_shell 'Running stage1 babushka...',
+              "arch-chroot /mnt babushka #{dependency.dep_source.name}:stage1"
+  }
 end
 
-dep 'chroot', :disk do
-  requires 'mounts'
+dep 'stage1.bootstrap', :disk do
+  requires 'babushka.bootstrap'.with(disk)
+
+  met? { '/mnt/root/.babushka/sources/karen/.git'.p.dir? }
+  meet { shell! "arch-chroot /mnt babushka sources --add #{dependency.dep_source.name} #{dependency.dep_source.uri}" }
+end
+
+dep 'babushka.bootstrap', :disk do
+  requires 'chroot.bootstrap'.with(disk)
+
+  met? { '/mnt/usr/local/babushka'.p.dir? }
+  meet {
+    log_shell 'Injecting babushka...',
+              'arch-chroot /mnt sh -c "`curl https://babushka.me/up`"'
+  }
+end
+
+dep 'chroot.bootstrap', :disk do
+  requires 'mounts.bootstrap'.with(disk)
 
   met? { shell? "arch-chroot /mnt true" }
   meet { log_shell 'Bootstrapping Arch Linux...', 'pacstrap /mnt base base-devel' }
 end
 
-dep 'filesystems', :disk do
+dep 'filesystems.bootstrap', :disk do
   requires 'boot.fs'.with(disk)
   requires 'swap.fs'.with(disk)
   requires 'root.fs'.with(disk)
 end
 
-dep 'mounts', :disk do
-  requires 'filesystems'
+dep 'mounts.bootstrap', :disk do
+  requires 'filesystems.bootstrap'
 
   requires 'root.mnt'.with(disk)
   requires 'boot.mnt'.with(disk)
