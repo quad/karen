945546949f6d2e97bbd017b64701cb2a6046f564
Scott Robinson <scott@quadhome.com>
2013-02-06 13:47:20 +0000

    Use partprobe instead of udevadm to trigger a /dev/disks rescan.

--- partition.rb
+++ partition.rb
@@ -23,7 +23,7 @@ meta 'partition' do
       attributes.each do |attr|
         sgdisk "--attributes=#{number}:set:#{attr}"
       end
-      shell! 'udevadm trigger'
+      shell 'partprobe'
     }
   }
 end
