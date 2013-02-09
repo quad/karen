8518b8acb5b84b4a7891214b4ec1e2736b360130
Scott Robinson <scott@quadhome.com>
2013-02-09 16:47:12 +0800

    Power management

--- stage2.rb
+++ stage2.rb
@@ -1,34 +1,48 @@
 dep 'stage2' do
-  dep 'text editor'
+  requires 'power management.stage2'
+  requires 'stage2.managed'
 end
 
-dep('text editor') { requires dep('vim.managed') }
+dep 'power management.stage2' do
+  requires dep('power management.managed') {
+    installs {
+      via :pacman,
+      'powertop',
+      'laptop-mode-tools'
+    }
+    provides ['powertop']
+  }
+
+  met? { shell? 'systemctl is-enabled laptop-mode.service' }
+  meet { shell 'systemctl enable laptop-mode.service' }
+end
 
 dep 'stage2.managed' do
   installs {
     via :pacman,
-	'alsa-utils',
-	'awesome',
-	'btrfs-progs',
-	'ca-certificates',
-	'go',
-	'iproute2',
-	'mesa',
-	'openssh',
-	'python',
-	'python2',
-	'ruby',
-	'rxvt-unicode',
-	'sudo',
-	'surf',
-	'tmux',
-	'ttf-dejavu',
-	'ttf-inconsolata',
-	'xf86-input-synaptics',
-	'xf86-video-intel',
-	'xorg-server',
-	'xorg-server-utils',
-	'xorg-xinit'
+        'vim'
+#	'alsa-utils',
+#	'awesome',
+#	'btrfs-progs',
+#	'ca-certificates',
+#	'go',
+#	'iproute2',
+#	'mesa',
+#	'openssh',
+#	'python',
+#	'python2',
+#	'ruby',
+#	'rxvt-unicode',
+#	'sudo',
+#	'surf',
+#	'tmux',
+#	'ttf-dejavu',
+#	'ttf-inconsolata',
+#	'xf86-input-synaptics',
+#	'xf86-video-intel',
+#	'xorg-server',
+#	'xorg-server-utils',
+#	'xorg-xinit'
   }
   provides []
 end
